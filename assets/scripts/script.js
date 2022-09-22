d9a80d0b65f591889dc7c90ee59391afa82460bb
Adam Gilroy <adamgilroy22@gmail.com>
2022-09-22 09:36:40 +0000

    Add code to log user's choice

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,23 @@
+const kickOutcome = document.querySelector("#kick-outcome");
+const controlButtons = document.querySelectorAll(".control-button");
+let userDirection;
 let cpuDirection;
+let outcome;
 
+/**
+ * Take the user's direction choice and assign the value to player
+ * and call the computer choice function
+ */
+controlButtons.forEach(button => button.addEventListener("click", () => {
 
+    userDirection = button.textContent;
+    generateCpuChoice();
 
-// Generate random number to determine what way the CPU shoots/dives
+}));
+
+/**
+ * Generate random number to determine what way the CPU shoots/dives
+ */
 function generateCpuDirection() {
     const cpuChoice = Math.floor(Math.random() * 3 + 1);
 
