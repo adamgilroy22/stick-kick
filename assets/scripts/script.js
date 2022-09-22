075946aa4dfa3cb417cae30efe0dc1bde15e0208
Adam Gilroy <adamgilroy22@gmail.com>
2022-09-22 14:22:45 +0000

    Add function to change kicker after each turn

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,15 @@
 const kickOutcome = document.querySelector("#kick-outcome");
 const controlButtons = document.querySelectorAll(".control-button");
-const userState = document.querySelector("#userState");
+const gameState = document.querySelector("#gameState");
 const userScore = document.getElementById('user-score');
 const cpuScore = document.getElementById('cpu-score');
+let userKicker;
+let cpuKicker;
 let userDirection;
 let cpuDirection;
 let userGoals = 0;
 let cpuGoals = 0;
+let kicker = userKicker;
 
 
 /**
@@ -52,6 +55,7 @@ function checkOutcome() {
         cpuGoals++;
         outcome = `User kicked ${userDirection} and CPU dived ${cpuDirection}. SAVED!`;
         cpuScore.innerHTML = cpuGoals;
+        changeKicker();
     } else {
         userGoals++;
         outcome = `User kicked ${userDirection} and CPU dived ${cpuDirection}. GOAL!`;
@@ -62,15 +66,26 @@ function checkOutcome() {
 }
 
 /**
+ * Change kicker after each turn
+ */
+function changeKicker() {
+    if (kicker === userKicker){
+        kicker = cpuKicker;
+    } else if (kicker === cpuKicker){
+        kicker = userKicker;
+    }
+}
+
+/**
  * Check if either player or CPU have reached 5 goals
  */
 function checkWinner() {
     if (userGoals === 5 && cpuGoals < 5){
-        userState.innerHTML = "YOU WIN!"
+        gameState.innerHTML = "YOU WIN!"
         endGame();
     }
     if (userGoals < 5 && cpuGoals === 5){
-        userState.innerHTML = "COMPUTER WINS!"
+        gameState.innerHTML = "COMPUTER WINS!"
         endGame();
     }
 }
@@ -79,10 +94,10 @@ function checkWinner() {
  * Disable direction buttons if either user or cpu reach 5 goals
  */
 function endGame() {
-    
-        document.getElementById("left").disabled = true;
-        document.getElementById("middle").disabled = true;
-        document.getElementById("right").disabled = true;
+
+       document.getElementById("left").disabled = true;
+       document.getElementById("middle").disabled = true;
+       document.getElementById("right").disabled = true;
     
 }
 
